786261126a6f33dcaa2fc48e6452daeed455f8a8
unknown <khorshedjhoni@gmail.com>
2022-11-30 00:05:49 +0600

    user add

--- index.js
+++ index.js
@@ -18,63 +18,66 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run(){
 try{
-    const categoriesCollection = client.db('recycleShop').collection('categories')
+    const serviceCollection = client.db('recycleShop').collection('categories')
     const usersCollection = client.db('recycleShop').collection('users')
+    const allMobile  = client.db('recycleShop').collection('allMobile')
+
+
 
     app.get('/categories', async (req, res) => {
         const query = {};
-        const result = await categoriesCollection.find(query).toArray();
+        const result = await serviceCollection.find(query).toArray();
         res.send(result);
     });
    
 
 
 
-    // //new
-    // app.post('/allmobile', async (req, res) => {
-    //     const review = req.body;
-    //     const result = await allMobile.insertOne(review);
-    //     res.send(result);
-    // });
-    // app.get('/allmobile', async (req, res) => {
-    //     let query = {};
-    //     const cursor = allMobile.find(query);
-    //     const serve = await cursor.toArray();
-    //     res.send(serve);
-    // });
+    //new
+    app.post('/allmobile', async (req, res) => {
+        const review = req.body;
+        const result = await allMobile.insertOne(review);
+        res.send(result);
+    });
+    app.get('/allmobile', async (req, res) => {
+        let query = {};
+        const cursor = allMobile.find(query);
+        const serve = await cursor.toArray();
+        res.send(serve);
+    });
 
-    // app.get('/allmobile/:id', async (req, res) => {
-    //     const id = req.params.id;
-    //     const query = { _id: ObjectId(id) };
-    //     const service = await serviceCollection.findOne(query);
-    //     res.send(service);
-    // });
+    app.get('/allmobile/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const service = await serviceCollection.findOne(query);
+        res.send(service);
+    });
 
-    // app.get('/reviewss', async (req, res) => {
-    //     let query = {};
+    app.get('/reviewss', async (req, res) => {
+        let query = {};
 
-    //     if (req.query.category) {
-    //         query = {
-    //             category: req.query.category
-    //         }
-    //     }
-    //     const cursor = allMobile.find(query).limit(0).sort({$natural:-1}) 
-    //     const review = await cursor.toArray();
-    //     res.send(review);
-    // });
+        if (req.query.category) {
+            query = {
+                category: req.query.category
+            }
+        }
+        const cursor = allMobile.find(query).limit(0).sort({$natural:-1}) 
+        const review = await cursor.toArray();
+        res.send(review);
+    });
 
 
-    // //jwt
-    // app.get('/jwt', async (req, res) => {
-    //     const email = req.query.email;
-    //     const query = { email: email };
-    //     const user = await usersCollection.findOne(query);
-    //     if (user) {
-    //         const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
-    //         return res.send({ accessToken: token });
-    //     }
-    //     res.status(403).send({ accessToken: '' })
-    // });
+    //jwt
+    app.get('/jwt', async (req, res) => {
+        const email = req.query.email;
+        const query = { email: email };
+        const user = await usersCollection.findOne(query);
+        if (user) {
+            const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+            return res.send({ accessToken: token });
+        }
+        res.status(403).send({ accessToken: '' })
+    });
 
 
     //users
@@ -85,24 +88,24 @@ try{
         res.send(result);
     });
 
-    // app.put('/users/admin/:id', async (req, res) => {
-    //     const id = req.params.id;
-    //     const filter = { _id: ObjectId(id) }
-    //     const options = { upsert: true };
-    //     const updatedDoc = {
-    //         $set: {
-    //             role: 'verified-seller'
-    //         }
-    //     }
-    //     const result = await usersCollection.updateOne(filter, updatedDoc, options);
-    //     res.send(result);
-    // });
+    app.put('/users/admin/:id', async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: ObjectId(id) }
+        const options = { upsert: true };
+        const updatedDoc = {
+            $set: {
+                role: 'verified-seller'
+            }
+        }
+        const result = await usersCollection.updateOne(filter, updatedDoc, options);
+        res.send(result);
+    });
 
-    // app.get('/users', async (req, res) => {
-    //     const query = {};
-    //     const users = await usersCollection.find(query).toArray();
-    //     res.send(users);
-    // });
+    app.get('/users', async (req, res) => {
+        const query = {};
+        const users = await usersCollection.find(query).toArray();
+        res.send(users);
+    });
 
 
 
